0c3340b543c8adc423a3613f38a30743c9537b53
Raffson <Raffson@users.noreply.github.com>
2022-11-12 12:41:50 +0100

    Set refueling probability to 10%

--- game/missiongenerator/aircraft/waypoints/refuel.py
+++ game/missiongenerator/aircraft/waypoints/refuel.py
@@ -1,10 +1,14 @@
 from dcs.point import MovingPoint
-from dcs.task import RefuelingTaskAction
+from dcs.task import RefuelingTaskAction, ControlledTask
 
 from .pydcswaypointbuilder import PydcsWaypointBuilder
 
 
 class RefuelPointBuilder(PydcsWaypointBuilder):
     def add_tasks(self, waypoint: MovingPoint) -> None:
-        waypoint.add_task(RefuelingTaskAction())
+        refuel = ControlledTask(
+            RefuelingTaskAction()
+        )
+        refuel.start_probability(10)
+        waypoint.add_task(refuel)
         return super().add_tasks(waypoint)
