ac81e73481a295233b4124dea3db1c9445bcc2ae
MetalStormGhost <ghosti@smytky.org>
2022-12-03 12:05:38 +0200

    Implemented GroundForcePainter for applying custom liveries to ground units. The liveries can be applied to front line objects (including infantry), convoys, naval units and TGOs.
    Livery overrides are defined in the faction file like this:
      "liveries_overrides_ground_forces": {
        "Soldier M249": [
          "summer",
          "DPM",
          "US-1",
          "US-2",
          "GOE III SPN m4"
        ],
        "Soldier M4": [
          "summer",
          "DPM",
          "US-1",
          "US-2",
          "GOE III SPN m4"
        ],
        "Soldier M4 GRG": [
          "BDU",
          "US Forest-1",
          "US Forest-2",
          "US Forest-3",
          "US Forest-4"
        ]
      }
    
    Note: the DCS id of the unit should be used, instead of the unit name defined in the unit yaml.

---
+++ game/missiongenerator/groundforcepainter.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import logging
+import random
+from typing import Any, Optional
+
+from dcs.unit import Ship
+from dcs.unitgroup import Vehicle
+
+from game.factions.faction import Faction
+
+
+class GroundForcePainter:
+    def __init__(self, faction: Faction, vehicle: Vehicle) -> None:
+        self.faction = faction
+        self.vehicle = vehicle
+
+    def livery_from_faction(self) -> Optional[str]:
+        faction = self.faction
+        try:
+            if (
+                choices := faction.liveries_overrides_ground_forces.get(
+                    self.vehicle.type
+                )
+            ) is not None:
+                return random.choice(choices)
+        except AttributeError:
+            logging.warning(
+                f"Faction {self.faction.name} is missing livery for ground unit {self.vehicle.type}"
+            )
+            return None
+        logging.warning(
+            f"Faction {self.faction.name} is missing livery for ground unit {self.vehicle.type}"
+        )
+        return None
+
+    def determine_livery(self) -> Optional[str]:
+        if (livery := self.livery_from_faction()) is not None:
+            return livery
+        return None
+
+    def apply_livery(self) -> None:
+        livery = self.determine_livery()
+        if livery is None:
+            return
+        self.vehicle.livery_id = livery
+
+
+class NavalForcePainter:
+    def __init__(self, faction: Faction, vessel: Ship) -> None:
+        self.faction = faction
+        self.vessel = vessel
+
+    def livery_from_faction(self) -> Optional[str]:
+        faction = self.faction
+        try:
+            if (
+                choices := faction.liveries_overrides_ground_forces.get(
+                    self.vessel.type
+                )
+            ) is not None:
+                return random.choice(choices)
+        except AttributeError:
+            logging.warning(
+                f"Faction {self.faction.name} is missing livery for naval unit {self.vessel.type}"
+            )
+            return None
+        logging.warning(
+            f"Faction {self.faction.name} is missing livery for naval unit {self.vessel.type}"
+        )
+        return None
+
+    def determine_livery(self) -> Optional[str]:
+        if (livery := self.livery_from_faction()) is not None:
+            return livery
+        return None
+
+    def apply_livery(self) -> None:
+        livery = self.determine_livery()
+        if livery is None:
+            return
+        self.vessel.livery_id = livery
