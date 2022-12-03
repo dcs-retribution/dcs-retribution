6c673697f0e26673d4c8df7b8d2df4aa6c3cbbea
Raffson <Raffson@users.noreply.github.com>
2022-12-03 21:28:13 +0100

    Fix livery overrides

--- qt_ui/windows/AirWingConfigurationDialog.py
+++ qt_ui/windows/AirWingConfigurationDialog.py
@@ -161,6 +161,14 @@ class SquadronLiverySelector(QComboBox):
             valid_livery = livery.countries is None or cc in livery.countries
             if valid_livery or cc in ["BLUE", "RED"]:
                 liveries.add(livery)
+        faction = squadron.coalition.faction
+        overrides = [
+            x
+            for x in faction.liveries_overrides.get(self.aircraft_type, [])
+            if x in [y.id.lower() for y in liveries]
+        ]
+        if len(overrides) > 0:
+            self.addItem("Use livery overrides", userData=None)
         for livery in sorted(liveries):
             self.addItem(livery.name, userData=livery.id)
             if selected_livery is not None:
