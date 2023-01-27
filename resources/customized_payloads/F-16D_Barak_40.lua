bb0bf66eac969d612849317f6f66a68ec20f8b8c
MetalStormGhost <ghosti@smytky.org>
2023-01-27 23:52:39 +0200

    Improved the F-16I Sufa & F-16D mod v3.2 loadouts.

--- resources/customized_payloads/F-16D_Barak_40.lua
+++ resources/customized_payloads/F-16D_Barak_40.lua
@@ -2,6 +2,58 @@ local unitPayloads = {
 	["name"] = "F-16D_Barak_40",
 	["payloads"] = {
 		[1] = {
+			["name"] = "Retribution OCA/Runway",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[4] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 8,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+				[6] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{34759BBC-AF1E-4AEE-A581-498FF7A6EBCE}",
+					["num"] = 3,
+				},
+				[9] = {
+					["CLSID"] = "{34759BBC-AF1E-4AEE-A581-498FF7A6EBCE}",
+					["num"] = 7,
+				},
+				[10] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[11] = {
+					["CLSID"] = "ALQ_184_Long",
+					["num"] = 13,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[2] = {
 			["name"] = "Retribution SEAD",
 			["pylons"] = {
 				[1] = {
@@ -17,7 +69,7 @@ local unitPayloads = {
 					["num"] = 3,
 				},
 				[4] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+					["CLSID"] = "{600gal}",
 					["num"] = 4,
 				},
 				[5] = {
@@ -25,7 +77,7 @@ local unitPayloads = {
 					["num"] = 5,
 				},
 				[6] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+					["CLSID"] = "{600gal}",
 					["num"] = 6,
 				},
 				[7] = {
@@ -48,12 +100,16 @@ local unitPayloads = {
 					["CLSID"] = "ALQ_184_Long",
 					["num"] = 13,
 				},
+				[12] = {
+					["CLSID"] = "{AN_ASQ_213}",
+					["num"] = 10,
+				},
 			},
 			["tasks"] = {
 				[1] = 29,
 			},
 		},
-		[2] = {
+		[3] = {
 			["name"] = "Retribution Ferry",
 			["pylons"] = {
 				[1] = {
@@ -73,11 +129,11 @@ local unitPayloads = {
 					["num"] = 2,
 				},
 				[5] = {
-					["CLSID"] = "MXU-648-TP",
+					["CLSID"] = "{600gal}",
 					["num"] = 6,
 				},
 				[6] = {
-					["CLSID"] = "MXU-648-TP",
+					["CLSID"] = "{600gal}",
 					["num"] = 4,
 				},
 				[7] = {
@@ -88,11 +144,225 @@ local unitPayloads = {
 					["CLSID"] = "ALQ_184_Long",
 					["num"] = 13,
 				},
+				[9] = {
+					["CLSID"] = "MXU-648-TP",
+					["num"] = 7,
+				},
+				[10] = {
+					["CLSID"] = "MXU-648-TP",
+					["num"] = 3,
+				},
 			},
 			["tasks"] = {
 			},
 		},
-		[3] = {
+		[4] = {
+			["name"] = "Retribution Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[4] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 8,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+				[6] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{GBU-31V3B}",
+					["num"] = 3,
+				},
+				[9] = {
+					["CLSID"] = "{GBU-31V3B}",
+					["num"] = 7,
+				},
+				[10] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[11] = {
+					["CLSID"] = "ALQ_184_Long",
+					["num"] = 13,
+				},
+			},
+			["tasks"] = {
+				[1] = 33,
+			},
+		},
+		[5] = {
+			["name"] = "ANTISHIP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 8,
+				},
+				[3] = {
+					["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",
+					["num"] = 7,
+				},
+				[4] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+				[6] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
+				},
+				[7] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",
+					["num"] = 3,
+				},
+				[9] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 2,
+				},
+				[10] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[11] = {
+					["CLSID"] = "ALQ_184_Long",
+					["num"] = 13,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+		[6] = {
+			["name"] = "Retribution SEAD Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "LAU_88_AGM_65H_2_R",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[10] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+				[11] = {
+					["CLSID"] = "ALQ_184_Long",
+					["num"] = 13,
+				},
+				[12] = {
+					["CLSID"] = "{AN_ASQ_213}",
+					["num"] = 10,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[7] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[10] = {
+					["CLSID"] = "ALQ_184_Long",
+					["num"] = 13,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+		[8] = {
 			["name"] = "CAS",
 			["pylons"] = {
 				[1] = {
@@ -143,106 +413,7 @@ local unitPayloads = {
 			["tasks"] = {
 			},
 		},
-		[4] = {
-			["name"] = "Retribution Strike",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 9,
-				},
-				[4] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 8,
-				},
-				[5] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 11,
-				},
-				[6] = {
-					["CLSID"] = "{GBU-24}",
-					["num"] = 4,
-				},
-				[7] = {
-					["CLSID"] = "{GBU-24}",
-					["num"] = 6,
-				},
-				[8] = {
-					["CLSID"] = "{GBU-24}",
-					["num"] = 3,
-				},
-				[9] = {
-					["CLSID"] = "{GBU-24}",
-					["num"] = 7,
-				},
-				[10] = {
-					["CLSID"] = "{IDF Mods Project 300gal}",
-					["num"] = 5,
-				},
-				[11] = {
-					["CLSID"] = "ALQ_184_Long",
-					["num"] = 13,
-				},
-			},
-			["tasks"] = {
-				[1] = 33,
-			},
-		},
-		[5] = {
-			["name"] = "CAP",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 3,
-				},
-				[4] = {
-					["CLSID"] = "{600gal}",
-					["num"] = 4,
-				},
-				[5] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 5,
-				},
-				[6] = {
-					["CLSID"] = "{600gal}",
-					["num"] = 6,
-				},
-				[7] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 7,
-				},
-				[8] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 8,
-				},
-				[9] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 9,
-				},
-				[10] = {
-					["CLSID"] = "ALQ_184_Long",
-					["num"] = 13,
-				},
-			},
-			["tasks"] = {
-			},
-		},
-		[6] = {
+		[9] = {
 			["displayName"] = "Retribution DEAD",
 			["name"] = "Retribution DEAD",
 			["pylons"] = {
@@ -297,161 +468,6 @@ local unitPayloads = {
 			},
 			["tasks"] = {
 				[1] = 31,
-			},
-		},
-		[7] = {
-			["name"] = "Retribution OCA/Runway",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 9,
-				},
-				[4] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 8,
-				},
-				[5] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 11,
-				},
-				[6] = {
-					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
-					["num"] = 4,
-				},
-				[7] = {
-					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
-					["num"] = 6,
-				},
-				[8] = {
-					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
-					["num"] = 3,
-				},
-				[9] = {
-					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
-					["num"] = 7,
-				},
-				[10] = {
-					["CLSID"] = "{IDF Mods Project 300gal}",
-					["num"] = 5,
-				},
-				[11] = {
-					["CLSID"] = "ALQ_184_Long",
-					["num"] = 13,
-				},
-			},
-			["tasks"] = {
-				[1] = 11,
-			},
-		},
-		[8] = {
-			["name"] = "ANTISHIP",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 9,
-				},
-				[2] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 8,
-				},
-				[3] = {
-					["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",
-					["num"] = 7,
-				},
-				[4] = {
-					["CLSID"] = "{600gal}",
-					["num"] = 6,
-				},
-				[5] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 11,
-				},
-				[6] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 5,
-				},
-				[7] = {
-					["CLSID"] = "{600gal}",
-					["num"] = 4,
-				},
-				[8] = {
-					["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",
-					["num"] = 3,
-				},
-				[9] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 2,
-				},
-				[10] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 1,
-				},
-				[11] = {
-					["CLSID"] = "ALQ_184_Long",
-					["num"] = 13,
-				},
-			},
-			["tasks"] = {
-			},
-		},
-		[9] = {
-			["name"] = "Retribution SEAD Escort",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "LAU_88_AGM_65H_2_L",
-					["num"] = 3,
-				},
-				[4] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
-					["num"] = 4,
-				},
-				[5] = {
-					["CLSID"] = "{IDF Mods Project 300gal}",
-					["num"] = 5,
-				},
-				[6] = {
-					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
-					["num"] = 6,
-				},
-				[7] = {
-					["CLSID"] = "LAU_88_AGM_65H_2_R",
-					["num"] = 7,
-				},
-				[8] = {
-					["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
-					["num"] = 8,
-				},
-				[9] = {
-					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
-					["num"] = 9,
-				},
-				[10] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 11,
-				},
-				[11] = {
-					["CLSID"] = "ALQ_184_Long",
-					["num"] = 13,
-				},
-			},
-			["tasks"] = {
-				[1] = 29,
 			},
 		},
 	},
