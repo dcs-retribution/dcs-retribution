dcs-liberation/dcs_liberation

Set units on the frontline to Hidden On MFD. (#2669)
All groups (friendly and enemy) that are part of the front line are set
to `Hidden On MFD`. This is a group level filter, and can not be applied
on a per unit basis.

game/missiongenerator/flotgenerator.py
@@ -215,7 +215,7 @@ class FlotGenerator:
                         u = random.choices(
                             manpads, weights=[m.spawn_weight for m in manpads]
                         )[0]
-                        self.mission.vehicle_group(
+                        infantry_group = self.mission.vehicle_group(
                             side,
                             namegen.next_infantry_name(side, u),
                             u.dcs_unit_type,
@@ -224,6 +224,7 @@ class FlotGenerator:
                             heading=forward_heading.degrees,
                             move_formation=PointAction.OffRoad,
                         )
+                        infantry_group.hidden_on_mfd = True
             return
 
         possible_infantry_units = set(faction.infantry_with_class(UnitClass.INFANTRY))
@@ -240,7 +241,7 @@ class FlotGenerator:
             weights=[u.spawn_weight for u in infantry_choices],
             k=INFANTRY_GROUP_SIZE,
         )
-        self.mission.vehicle_group(
+        infantry_group = self.mission.vehicle_group(
             side,
             namegen.next_infantry_name(side, units[0]),
             units[0].dcs_unit_type,
@@ -249,10 +250,11 @@ class FlotGenerator:
             heading=forward_heading.degrees,
             move_formation=PointAction.OffRoad,
         )
+        infantry_group.hidden_on_mfd = True
 
         for unit in units[1:]:
             position = infantry_position.random_point_within(55, 5)
-            self.mission.vehicle_group(
+            infantry_group = self.mission.vehicle_group(
                 side,
                 namegen.next_infantry_name(side, unit),
                 unit.dcs_unit_type,
@@ -261,6 +263,7 @@ class FlotGenerator:
                 heading=forward_heading.degrees,
                 move_formation=PointAction.OffRoad,
             )
+            infantry_group.hidden_on_mfd = True
 
     def _set_reform_waypoint(
         self, dcs_group: VehicleGroup, forward_heading: Heading
@@ -771,6 +774,7 @@ class FlotGenerator:
             group_size=count,
             heading=heading.degrees,
         )
+        group.hidden_on_mfd = True
 
         self.unit_map.add_front_line_units(group, cp, unit_type)
 

dcs-retribution/dcs-retribution

game/missiongenerator/flotgenerator.py
@@ -234,6 +234,7 @@ class FlotGenerator:
                         )
                         vehicle = vg.units[0]
                         GroundForcePainter(faction, vehicle).apply_livery()
+                        vg.hidden_on_mfd = True
             return
 
         possible_infantry_units = set(faction.infantry_with_class(UnitClass.INFANTRY))
@@ -261,6 +262,7 @@ class FlotGenerator:
         )
         vehicle = vg.units[0]
         GroundForcePainter(faction, vehicle).apply_livery()
+        vg.hidden_on_mfd = True
 
         for unit in units[1:]:
             position = infantry_position.random_point_within(55, 5)
@@ -275,6 +277,7 @@ class FlotGenerator:
             )
             vehicle = vg.units[0]
             GroundForcePainter(faction, vehicle).apply_livery()
+            vg.hidden_on_mfd = True
 
     def _earliest_tot_on_flot(self, player: bool) -> timedelta:
         tots = [
@@ -645,7 +648,7 @@ class FlotGenerator:
     ) -> List[VehicleGroup]:
         """
         Return the nearest enemy group for the player group
-        @param group Group for which we should find the nearest ennemies
+        @param player_group Group for which we should find the nearest ennemies
         @param enemy_groups Potential enemy groups
         @param n number of nearby groups to take
         """
@@ -670,7 +673,7 @@ class FlotGenerator:
     ) -> Optional[VehicleGroup]:
         """
         Search the enemy groups for a potential target suitable to armored assault
-        @param group Group for which we should find the nearest ennemy
+        @param player_group Group for which we should find the nearest ennemy
         @param enemy_groups Potential enemy groups
         """
         min_distance = math.inf
@@ -813,6 +816,7 @@ class FlotGenerator:
             group_size=count,
             heading=heading.degrees,
         )
+        group.hidden_on_mfd = True
 
         self.unit_map.add_front_line_units(group, cp, unit_type)
 