51fc0215d69974bfa12d49776a9b9fd79341643e
Raffson <Raffson@users.noreply.github.com>
2023-06-25 03:21:58 +0200

    Link landing wpt to pad in flightgroupspawner
    Fixes a bug where helipads would try to pop from an empty list

--- game/missiongenerator/aircraft/flightgroupspawner.py
+++ game/missiongenerator/aircraft/flightgroupspawner.py
@@ -9,7 +9,7 @@ from dcs.mission import StartType as DcsStartType
 from dcs.planes import F_14A, Su_33
 from dcs.point import PointAction
 from dcs.ships import KUZNECOW
-from dcs.terrain import Airport, NoParkingSlotError
+from dcs.terrain import NoParkingSlotError
 from dcs.unitgroup import FlyingGroup, ShipGroup, StaticGroup
 
 from game.ato import Flight
@@ -309,6 +309,13 @@ class FlightGroupSpawner:
         if self.start_type is not StartType.COLD:
             group.points[0].action = PointAction.FromGroundAreaHot
             group.points[0].type = "TakeOffGroundHot"
+
+        wpt = group.waypoint("LANDING")
+        if wpt:
+            hpad = self.helipads[self.flight.arrival].pop(0)
+            wpt.helipad_id = hpad.units[0].id
+            wpt.link_unit = hpad.units[0].id
+            self.helipads[self.flight.arrival].append(hpad)
 
         for i in range(self.flight.count - 1):
             try:
