53f03bff9643d230b875890dedc8246a683be4c0
Turbolious <39207917+Turbolious@users.noreply.github.com>
2023-07-19 19:43:01 -0400

    Aim-9 corrections
    Year and fallback corrections

---
+++ resources/customized_payloads/VSN_F4E.lua
@@ -0,0 +1,181 @@
+local unitPayloads = {
+	["name"] = "VSN_F4E",
+	["payloads"] = {
+		[1] = {
+			["name"] = "Retribution BARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{9DDF5297-94B9-42FC-A45E-6E316121CD85}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{9DDF5297-94B9-42FC-A45E-6E316121CD85}",
+					["num"] = 9,
+				},
+				[3] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{8B9E3FD0-F034-4A07-B6CE-C269884CC71B}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 7,
+				},
+				[7] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 2,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 10,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+		[2] = {
+			["name"] = "Retribution TARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 10,
+				},
+				[3] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 5,
+				},
+				[7] = {
+					["CLSID"] = "{8B9E3FD0-F034-4A07-B6CE-C269884CC71B}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 7,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+		[3] = {
+			["name"] = "Retribution Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6D21ECEA-F85B-4E8D-9D51-31DC9B8AA4EF}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{8B9E3FD0-F034-4A07-B6CE-C269884CC71B}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 7,
+				},
+				[7] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 9,
+				},
+				[9] = {
+					["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",
+					["num"] = 10,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+		[4] = {
+			["name"] = "Retribution SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{3E6B632D-65EB-44D2-9501-1C2D04515405}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{3E6B632D-65EB-44D2-9501-1C2D04515405}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{6D21ECEA-F85B-4E8D-9D51-31DC9B8AA4EF}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{8B9E3FD0-F034-4A07-B6CE-C269884CC71B}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 7,
+				},
+				[7] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "{3E6B632D-65EB-44D2-9501-1C2D04515405}",
+					["num"] = 9,
+				},
+				[9] = {
+					["CLSID"] = "{3E6B632D-65EB-44D2-9501-1C2D04515405}",
+					["num"] = 10,
+				},
+			},
+			["tasks"] = {
+			},
+		},
+	},
+	["tasks"] = {
+	},
+	["unitType"] = "VSN_F4E",
+}
+return unitPayloads
