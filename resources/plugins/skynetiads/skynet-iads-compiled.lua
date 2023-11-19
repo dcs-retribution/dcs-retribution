7d95e3e2fe9e46542f8542f9115523e20f7357cc
Raffson <Raffson@users.noreply.github.com>
2023-11-19 22:26:53 +0100

    Add new units to skynet script

--- resources/plugins/skynetiads/skynet-iads-compiled.lua
+++ resources/plugins/skynetiads/skynet-iads-compiled.lua
@@ -47,9 +47,16 @@ samTypesDB = {
 					['NATO'] = 'Big Bird',
 				},
 			},
+			['S-300PS 40B6MD sr_19J6'] = {
+				['name'] = {
+					['NATO'] = 'Tin Shield',
+				},
+			},
 		},
 		['trackingRadar'] = {
 			['S-300PS 40B6M tr'] = {
+			},
+			['S-300PS 5H63C 30H6_tr'] = {
 			},
 		},
 		['launchers'] = {
@@ -396,6 +403,22 @@ samTypesDB = {
 			['NATO'] = 'CSA-4',
 		},
 		['harm_detection_chance'] = 30
+	},
+	['HEMTT_C-RAM_Phalanx'] = {
+		['type'] = 'single',
+		['searchRadar'] = {
+			['HEMTT_C-RAM_Phalanx'] = {
+			},
+		},
+		['launchers'] = {
+			['HEMTT_C-RAM_Phalanx'] = {
+			},
+		},
+		['name'] = {
+			['NATO'] = 'CIWS',
+		},
+		['harm_detection_chance'] = 70,
+		['can_engage_harm'] = true
 	},
 --- Start of EW radars:
 	['1L13 EWR'] = {
