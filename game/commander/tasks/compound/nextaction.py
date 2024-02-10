14a0310d2590d132a174c93ac5eca21ba2066148
Raffson <Raffson@users.noreply.github.com>
2024-02-10 20:42:01 +0100

    Prioritize degrading IADS

--- game/commander/tasks/compound/nextaction.py
+++ game/commander/tasks/compound/nextaction.py
@@ -25,10 +25,10 @@ class PlanNextAction(CompoundTask[TheaterState]):
     def each_valid_method(self, state: TheaterState) -> Iterator[Method[TheaterState]]:
         yield [TheaterSupport()]
         yield [ProtectAirSpace()]
-        yield [CaptureBases()]
         yield [DefendBases()]
         yield [InterdictReinforcements()]
+        yield [DegradeIads()]
         yield [AttackBattlePositions()]
+        yield [CaptureBases()]
         yield [AttackAirInfrastructure(self.aircraft_cold_start)]
         yield [AttackBuildings()]
-        yield [DegradeIads()]
