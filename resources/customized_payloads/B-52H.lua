06242a433c46f4e301d139b1e33fdf33babb7255
Starfire13 <72491792+Starfire13@users.noreply.github.com>
2024-02-11 23:12:41 +1100

    Update B-52 loadout after ED's recent rework of the aircraft (#250)
    * Add files via upload
    
    * Use Mk82s exclusively in OCA/Runway payload
    
    Combining different types of dumb bombs isn't quite supported by the AI (yet)
    
    ---------
    
    Co-authored-by: Raffson <Raffson@users.noreply.github.com>

--- resources/customized_payloads/B-52H.lua
+++ resources/customized_payloads/B-52H.lua
@@ -2,84 +2,81 @@ local unitPayloads = {
 	["name"] = "B-52H",
 	["payloads"] = {
 		[1] = {
-			["name"] = "ANTISHIP",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{46ACDCF8-5451-4E26-BDDB-E78D5830E93C}",
-					["num"] = 2,
-				},
-			},
-			["tasks"] = {
-				[1] = 30,
-			},
-		},
-		[2] = {
-			["name"] = "STRIKE",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 3,
-				},
-			},
-			["tasks"] = {
-				[1] = 32,
-				[2] = 34,
-			},
-		},
-		[3] = {
-			["name"] = "SEAD",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{8DCAF3A3-7FCF-41B8-BB88-58DEDA878EDE}",
-					["num"] = 2,
-				},
-			},
-			["tasks"] = {
-				[1] = 33,
-			},
-		},
-		[4] = {
-			["name"] = "CAP",
-			["pylons"] = {
-			},
-			["tasks"] = {
-			},
-		},
-		[5] = {
-			["name"] = "CAS",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 3,
-				},
-				[2] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-			},
-		},
-		[6] = {
-			["displayName"] = "Retribution OCA/Runway",
 			["name"] = "Retribution OCA/Runway",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 1,
+					["CLSID"] = "{585D626E-7F42-4073-AB70-41E728C333E2}",
+					["num"] = 3,
 				},
 				[2] = {
-					["CLSID"] = "{696CFFC4-0BDE-42A8-BE4B-0BE3D9DD723C}",
-					["num"] = 3,
+					["CLSID"] = "{6C47D097-83FF-4FB2-9496-EAB36DDF0B05}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{585D626E-7F42-4073-AB70-41E728C333E2}",
+					["num"] = 1,
 				},
 			},
 			["tasks"] = {
 				[1] = 32,
-				[2] = 34,
+			},
+		},
+		[2] = {
+			["name"] = "Retribution DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{8DCAF3A3-7FCF-41B8-BB88-58DEDA878EDE}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[3] = {
+			["displayName"] = "Retribution Anti-ship",
+			["name"] = "Retribution Anti-ship",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{HSAB-6xAGM-84}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{HSAB-6xAGM-84}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[4] = {
+			["displayName"] = "Retribution Strike",
+			["name"] = "Retribution Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{585D626E-7F42-4073-AB70-41E728C333E2}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{6C47D097-83FF-4FB2-9496-EAB36DDF0B05}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{585D626E-7F42-4073-AB70-41E728C333E2}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
 			},
 		},
 	},
