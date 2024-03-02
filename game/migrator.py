d2004e5b11b942b08ffa8601125c0460d374aba9
Raffson <Raffson@users.noreply.github.com>
2024-03-03 00:27:20 +0100

    Clear parking slots when save is loaded

--- game/migrator.py
+++ game/migrator.py
@@ -78,6 +78,8 @@ class Migrator:
     def _update_control_points(self) -> None:
         is_sinai = self.game.theater.terrain.name == "SinaiMap"
         for cp in self.game.theater.controlpoints:
+            for parking_slot in cp.parking_slots:
+                parking_slot.unit_id = None
             is_carrier = cp.is_carrier
             is_lha = cp.is_lha
             is_fob = cp.category == "fob"
