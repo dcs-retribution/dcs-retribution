dd2fb432f2d185ee8e25ac0878c3c73775963fb7
Raffson <Raffson@users.noreply.github.com>
2024-04-01 16:03:40 +0200

    Use initial point for Herc AirAssault ingress

--- game/ato/flightplans/airassault.py
+++ game/ato/flightplans/airassault.py
@@ -136,10 +136,18 @@ class Builder(FormationAttackBuilder[AirAssaultFlightPlan, AirAssaultLayout]):
             pickup.alt = altitude
             pickup_position = pickup.position
 
-        ingress = builder.ingress(
-            FlightWaypointType.INGRESS_AIR_ASSAULT,
-            self.package.waypoints.ingress,
-            self.package.target,
+        ingress = (
+            builder.ingress(
+                FlightWaypointType.INGRESS_AIR_ASSAULT,
+                self.package.waypoints.ingress,
+                self.package.target,
+            )
+            if not self.flight.is_hercules
+            else builder.ingress(
+                FlightWaypointType.INGRESS_AIR_ASSAULT,
+                self.package.waypoints.initial,
+                self.package.target,
+            )
         )
 
         assault_area = builder.assault_area(self.package.target)
