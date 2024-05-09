a27663e4b68b6a80f11a68bd2607a4b93157b6a6
MetalStormGhost <89945461+MetalStormGhost@users.noreply.github.com>
2024-05-09 13:19:30 +0300

    Default start type for player flights (#303)
    * Implemented a new option in settings: Default start type for Player flights.
    
    * Updated changelog.
    
    * Removed unnecessary country parameter.
    
    * Restore missing parameter
    
    * on_pilot_changed should emit pilots_changed in its finally block, otherwise the start-type isn't updated if you have a single client pilot which you switch to a non-client pilot.
    
    Also implemented other changes suggested by @Raffson, such as a more streamlined start_type QComboBox handling and moving the pilots_changed Signal to FlightRosterEditor.
    
    * Decouple Signal from QFlighStartType
    
    ---------
    
    Co-authored-by: Raffson <Raffson@users.noreply.github.com>

--- game/settings/settings.py
+++ game/settings/settings.py
@@ -739,6 +739,14 @@ class Settings:
             "will not be included in automatically planned OCA packages."
         ),
     )
+    default_start_type_client: StartType = choices_option(
+        "Default start type for Player flights",
+        page=MISSION_GENERATOR_PAGE,
+        section=GAMEPLAY_SECTION,
+        choices={v.value: v for v in StartType},
+        default=StartType.COLD,
+        detail=("Default start type for flights containing Player/Client slots."),
+    )
     nevatim_parking_fix: bool = boolean_option(
         "Force air-starts for aircraft at Nevatim and Ramon Airbase inoperable parking slots",
         page=MISSION_GENERATOR_PAGE,
