92db0fe21127b92cf0366899233ef2a5917fa20f
MetalStormGhost <ghosti@smytky.org>
2024-06-06 23:46:21 +0300

    Updated the EA-6B Prowler pydcs extension to v2.9.4.102 of the mod.

--- pydcs_extensions/ea6b/ea6b.py
+++ pydcs_extensions/ea6b/ea6b.py
@@ -25,96 +25,81 @@ inject_weapons(WeaponsEA6B)
 @planemod
 class EA_6B(PlaneType):
     id = "EA_6B"
-    group_size_max = 1
-    height = 6.93
-    width = 20.93
-    length = 16.26
-    fuel_max = 5500
-    max_speed = 834.12
+    height = 4.57
+    width = 10.15
+    length = 17.98
+    fuel_max = 6994
+    max_speed = 1047.96
     chaff = 30
     flare = 30
     charge_total = 60
     chaff_charge_size = 1
     flare_charge_size = 1
     eplrs = True
-    radio_frequency = 127.5
+    radio_frequency = 250.5
 
     livery_name = "EA_6B"  # from type
 
     class Pylon1:
-        LAU_118a_with_AGM_45B_Shrike_ARM__Imp_ = (
-            1,
-            Weapons.LAU_118a_with_AGM_45B_Shrike_ARM__Imp_,
-        )
+        LAU_118A___AGM_45B_Shrike_ARM = (1, Weapons.LAU_118A___AGM_45B_Shrike_ARM)
         AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_ = (
             1,
             Weapons.AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_,
         )
-        Fuel_tank_S_3 = (1, Weapons.Fuel_tank_S_3)
-        EA6B_AN_ALQ_99 = (1, WeaponsEA6B.EA6B_AN_ALQ_99)
+        F_5_275Gal_Fuel_tank = (1, Weapons.F_5_275Gal_Fuel_tank)
+        EA6B_AN_ALQ_99 = (1, Weapons.EA6B_AN_ALQ_99)
 
     # ERRR <CLEAN>
 
     class Pylon2:
-        LAU_118a_with_AGM_45B_Shrike_ARM__Imp_ = (
-            2,
-            Weapons.LAU_118a_with_AGM_45B_Shrike_ARM__Imp_,
-        )
+        LAU_118A___AGM_45B_Shrike_ARM = (2, Weapons.LAU_118A___AGM_45B_Shrike_ARM)
         AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_ = (
             2,
             Weapons.AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_,
         )
-        Fuel_tank_S_3 = (2, Weapons.Fuel_tank_S_3)
-        EA6B_AN_ALQ_99 = (2, WeaponsEA6B.EA6B_AN_ALQ_99)
+        F_5_275Gal_Fuel_tank = (2, Weapons.F_5_275Gal_Fuel_tank)
+        EA6B_AN_ALQ_99 = (2, Weapons.EA6B_AN_ALQ_99)
 
     # ERRR <CLEAN>
 
     class Pylon3:
-        Fuel_tank_S_3 = (3, Weapons.Fuel_tank_S_3)
-        EA6B_AN_ALQ_99_ = (3, WeaponsEA6B.EA6B_AN_ALQ_99_)
+        F_5_275Gal_Fuel_tank = (3, Weapons.F_5_275Gal_Fuel_tank)
+        EA6B_AN_ALQ_99_ = (3, Weapons.EA6B_AN_ALQ_99_)
 
     # ERRR <CLEAN>
 
     class Pylon4:
-        LAU_118a_with_AGM_45B_Shrike_ARM__Imp_ = (
-            4,
-            Weapons.LAU_118a_with_AGM_45B_Shrike_ARM__Imp_,
-        )
+        LAU_118A___AGM_45B_Shrike_ARM = (4, Weapons.LAU_118A___AGM_45B_Shrike_ARM)
         AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_ = (
             4,
             Weapons.AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_,
         )
-        Fuel_tank_S_3 = (4, Weapons.Fuel_tank_S_3)
-        EA6B_AN_ALQ_99 = (4, WeaponsEA6B.EA6B_AN_ALQ_99)
+        F_5_275Gal_Fuel_tank = (4, Weapons.F_5_275Gal_Fuel_tank)
+        EA6B_AN_ALQ_99 = (4, Weapons.EA6B_AN_ALQ_99)
 
     # ERRR <CLEAN>
 
     class Pylon5:
-        LAU_118a_with_AGM_45B_Shrike_ARM__Imp_ = (
-            5,
-            Weapons.LAU_118a_with_AGM_45B_Shrike_ARM__Imp_,
-        )
+        LAU_118A___AGM_45B_Shrike_ARM = (5, Weapons.LAU_118A___AGM_45B_Shrike_ARM)
         AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_ = (
             5,
             Weapons.AGM_88C_HARM___High_Speed_Anti_Radiation_Missile_,
         )
-        Fuel_tank_S_3 = (5, Weapons.Fuel_tank_S_3)
-        EA6B_AN_ALQ_99 = (5, WeaponsEA6B.EA6B_AN_ALQ_99)
+        F_5_275Gal_Fuel_tank = (5, Weapons.F_5_275Gal_Fuel_tank)
+        EA6B_AN_ALQ_99 = (5, Weapons.EA6B_AN_ALQ_99)
 
     # ERRR <CLEAN>
 
     pylons = {1, 2, 3, 4, 5}
 
     tasks = [
-        task.CAP,
         task.Escort,
-        task.FighterSweep,
-        task.Intercept,
         task.Reconnaissance,
         task.GroundAttack,
         task.CAS,
         task.AFAC,
         task.RunwayAttack,
         task.AntishipStrike,
+        task.SEAD,
     ]
     task_default = task.GroundAttack
