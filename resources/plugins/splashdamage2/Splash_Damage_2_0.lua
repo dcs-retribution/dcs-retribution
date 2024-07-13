739405d716064846a2ea2face356fb6b8c3d7a48
Raffson <Raffson@users.noreply.github.com>
2024-07-14 01:01:44 +0200

    Add work-around for SplashDamage errors

--- resources/plugins/splashdamage2/Splash_Damage_2_0.lua
+++ resources/plugins/splashdamage2/Splash_Damage_2_0.lua
@@ -541,6 +541,20 @@ function track_wpns()
 end
 
 function onWpnEvent(event)
+  --[[
+    What follows is a work-around for what is presumed to be a DCS-bug:
+    https://forum.dcs.world/topic/353679-possible-bug-in-objectgettypename-during-s_event_kill/
+  ]]--
+  if event.weapon then
+    local status, retval = pcall(event.weapon.getTypeName, event.weapon)
+    if not status then
+      -- gameMsg(tostring(event.id)..'  WTF?\n'..tostring(retval))
+      -- gameMsg(mist.utils.tableShow(event))
+      -- gameMsg(tostring(event.weapon.getTypeName))
+      return
+    end
+  end
+  -- end of work-around
   if event.weapon and ignoredWeaps[event.weapon:getTypeName()] then
     return
   end
