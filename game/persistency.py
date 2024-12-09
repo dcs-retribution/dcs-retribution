83752d8140a45940dd7936c9929313cf21d08e83
Eclipse/Druss99 <dalton.w.russell@gmail.com>
2024-12-09 16:37:20 -0500

    cleanup code

--- game/persistency.py
+++ game/persistency.py
@@ -169,10 +169,6 @@ def kneeboards_dir() -> Path:
     return base_path() / "Retribution" / "Kneeboards"
 
 
-def mission_editor_dir() -> Path:
-    return base_path() / "MissionEditor"
-
-
 def payloads_dir(backup: bool = False) -> Path:
     payloads = base_path() / "MissionEditor" / "UnitPayloads"
     if backup:
