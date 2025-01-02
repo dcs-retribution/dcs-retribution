933b741d4736e0a516e1243d3ca4e7a1289bff8f
Astro-739 <145295326+Astro-739@users.noreply.github.com>
2025-01-02 15:38:25 +0100

    add strike and cas tasks to ra-5c

--- resources/customized_payloads/vwv_ra-5.lua
+++ resources/customized_payloads/vwv_ra-5.lua
@@ -13,6 +13,33 @@ local unitPayloads = {
 			{
 				[1]=17
 			}
+		},
+		[2]=
+		{
+			["displayName"]="Retribution Strike",
+			["name"]="Retribution Strike",
+			["pylons"]=
+			{
+			},
+			["tasks"]=
+			{
+				[1]=31,
+				[2]=32,
+				[3]=33
+			}
+		},
+		[3]=
+		{
+			["displayName"]="Retribution CAS",
+			["name"]="Retribution CAS",
+			["pylons"]=
+			{
+			},
+			["tasks"]=
+			{
+				[1]=31,
+				[2]=16
+			}
 		}
 	},
 	["unitType"]="vwv_ra-5"
