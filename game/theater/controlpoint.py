6046a0a9b756866c3d2e9d62df02715e5ca236ae
Astro-739 <145295326+Astro-739@users.noreply.github.com>
2025-01-03 11:15:20 +0100

    black reformat controlpoint.py

--- game/theater/controlpoint.py
+++ game/theater/controlpoint.py
@@ -527,8 +527,7 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
 
     @property
     @abstractmethod
-    def heading(self) -> Heading:
-        ...
+    def heading(self) -> Heading: ...
 
     def __str__(self) -> str:
         return self.name
@@ -692,8 +691,7 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
 
     @property
     @abstractmethod
-    def can_deploy_ground_units(self) -> bool:
-        ...
+    def can_deploy_ground_units(self) -> bool: ...
 
     @abstractmethod
     def total_aircraft_parking(self, parking_type: ParkingType) -> int:
@@ -988,8 +986,7 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
         return None
 
     @abstractmethod
-    def can_operate(self, aircraft: AircraftType) -> bool:
-        ...
+    def can_operate(self, aircraft: AircraftType) -> bool: ...
 
     def unclaimed_parking(self, parking_type: ParkingType) -> int:
         return (
@@ -1003,8 +1000,7 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
         theater: ConflictTheater,
         conditions: Conditions,
         dynamic_runways: Dict[str, RunwayData],
-    ) -> RunwayData:
-        ...
+    ) -> RunwayData: ...
 
     def stub_runway_data(self) -> RunwayData:
         return RunwayData(
@@ -1021,13 +1017,11 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
 
     @property
     @abstractmethod
-    def runway_is_destroyable(self) -> bool:
-        ...
+    def runway_is_destroyable(self) -> bool: ...
 
     @property
     @abstractmethod
-    def runway_status(self) -> RunwayStatus:
-        ...
+    def runway_status(self) -> RunwayStatus: ...
 
     @property
     def runway_can_be_repaired(self) -> bool:
@@ -1201,13 +1195,11 @@ class ControlPoint(MissionTarget, SidcDescribable, ABC):
 
     @property
     @abstractmethod
-    def category(self) -> str:
-        ...
+    def category(self) -> str: ...
 
     @property
     @abstractmethod
-    def status(self) -> ControlPointStatus:
-        ...
+    def status(self) -> ControlPointStatus: ...
 
 
 class Airfield(ControlPoint, CTLD):
