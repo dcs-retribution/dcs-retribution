703e4be2f32465e220c12a7c8d44972a1bb376df
Starfire13 <72491792+Starfire13@users.noreply.github.com>
2025-11-09 10:46:54 +1100

    Add files via upload

--- resources/customized_payloads/B-52H.lua
+++ resources/customized_payloads/B-52H.lua
@@ -2,6 +2,84 @@ local unitPayloads = {
 	["name"] = "B-52H",
 	["payloads"] = {
 		[1] = {
+			["displayName"] = "Retribution BAI",
+			["name"] = "Retribution BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{HSAB_8x_CBU105_R}",
+					["num"] = 3,
+					["settings"] = {
+						["NFP_fuze_type_nose"] = "FZU39",
+						["function_altitude_ctrl_FZU39_SUU65_SFW"] = 457.2,
+						["function_delay_ctrl_FZU39_SUU65_SFW"] = 2.23,
+					},
+				},
+				[2] = {
+					["CLSID"] = "{HSAB_8x_CBU105_L}",
+					["num"] = 1,
+					["settings"] = {
+						["NFP_PRESID"] = "MDRN_CC_A_SUU65Plus_SFW",
+						["NFP_PRESVER"] = 1,
+						["NFP_fuze_type_nose"] = "FZU39",
+						["function_altitude_ctrl_FZU39_SUU65_SFW"] = 457.2,
+						["function_delay_ctrl_FZU39_SUU65_SFW"] = 2.23,
+					},
+				},
+				[3] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[2] = {
+			["name"] = "Retribution DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{8DCAF3A3-7FCF-41B8-BB88-58DEDA878EDE}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 1,
+				},
+				[4] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[3] = {
+			["displayName"] = "Retribution Anti-ship",
+			["name"] = "Retribution Anti-ship",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{HSAB_4x_AGM84D_R}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{HSAB_4x_AGM84D_L}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[4] = {
 			["name"] = "Retribution OCA/Runway",
 			["pylons"] = {
 				[1] = {
@@ -35,49 +113,16 @@ local unitPayloads = {
 						["NFP_fuze_type_tail"] = "FMU143",
 					},
 				},
-			},
-			["tasks"] = {
-				[1] = 32,
-			},
-		},
-		[2] = {
-			["name"] = "Retribution DEAD",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
-					["num"] = 3,
-				},
-				[2] = {
-					["CLSID"] = "{8DCAF3A3-7FCF-41B8-BB88-58DEDA878EDE}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
-					["num"] = 1,
+				[4] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
 				},
 			},
 			["tasks"] = {
 				[1] = 32,
 			},
 		},
-		[3] = {
-			["displayName"] = "Retribution Anti-ship",
-			["name"] = "Retribution Anti-ship",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{HSAB_4x_AGM84D_R}",
-					["num"] = 3,
-				},
-				[2] = {
-					["CLSID"] = "{HSAB_4x_AGM84D_L}",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-				[1] = 32,
-			},
-		},
-		[4] = {
+		[5] = {
 			["displayName"] = "Retribution Strike",
 			["name"] = "Retribution Strike",
 			["pylons"] = {
@@ -112,12 +157,16 @@ local unitPayloads = {
 						["NFP_fuze_type_tail"] = "FMU143",
 					},
 				},
+				[4] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
+				},
 			},
 			["tasks"] = {
 				[1] = 32,
 			},
 		},
-		[5] = {
+		[6] = {
 			["displayName"] = "Retribution OCA/Aircraft",
 			["name"] = "Retribution OCA/Aircraft",
 			["pylons"] = {
@@ -152,34 +201,9 @@ local unitPayloads = {
 						["NFP_fuze_type_tail"] = "FMU143",
 					},
 				},
-			},
-			["tasks"] = {
-				[1] = 32,
-			},
-		},
-		[6] = {
-			["displayName"] = "Retribution BAI",
-			["name"] = "Retribution BAI",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{HSAB_8x_CBU105_R}",
-					["num"] = 3,
-					["settings"] = {
-						["NFP_fuze_type_nose"] = "FZU39",
-						["function_altitude_ctrl_FZU39_SUU65_SFW"] = 457.2,
-						["function_delay_ctrl_FZU39_SUU65_SFW"] = 2.23,
-					},
-				},
-				[2] = {
-					["CLSID"] = "{HSAB_8x_CBU105_L}",
-					["num"] = 1,
-					["settings"] = {
-						["NFP_PRESID"] = "MDRN_CC_A_SUU65Plus_SFW",
-						["NFP_PRESVER"] = 1,
-						["NFP_fuze_type_nose"] = "FZU39",
-						["function_altitude_ctrl_FZU39_SUU65_SFW"] = 457.2,
-						["function_delay_ctrl_FZU39_SUU65_SFW"] = 2.23,
-					},
+				[4] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 4,
 				},
 			},
 			["tasks"] = {
