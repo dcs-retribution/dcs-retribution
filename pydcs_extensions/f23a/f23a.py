dd7164e6241f5c9d9351666f7cc06cddf021df49
JaxLam16 <jrlamanna@icloud.com>
2026-04-03 22:46:11 -0500

    Format f23a.py with black

--- pydcs_extensions/f23a/f23a.py
+++ pydcs_extensions/f23a/f23a.py
@@ -22,7 +22,7 @@ class F_23A(PlaneType):
     chaff_charge_size = 1
     flare_charge_size = 2
     eplrs = True
-    category = "Interceptor"  #{78EFB7A2-FD52-4b57-A6A6-3BF0E1D6555F}
+    category = "Interceptor"  # {78EFB7A2-FD52-4b57-A6A6-3BF0E1D6555F}
     radio_frequency = 124
 
     livery_name = "F-23A"  # from type
@@ -32,22 +32,40 @@ class F_23A(PlaneType):
         AIM_9M_Sidewinder_IR_AAM = (1, Weapons.AIM_9M_Sidewinder_IR_AAM)
 
     class Pylon3:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (3, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            3,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon4:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (4, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            4,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon5:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (5, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            5,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon6:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (6, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            6,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon7:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (7, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            7,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon8:
-        AIM_120C_AMRAAM___Active_Radar_AAM = (8, Weapons.AIM_120C_AMRAAM___Active_Radar_AAM)
+        AIM_120C_AMRAAM___Active_Radar_AAM = (
+            8,
+            Weapons.AIM_120C_AMRAAM___Active_Radar_AAM,
+        )
 
     class Pylon10:
         AIM_9M_Sidewinder_IR_AAM = (10, Weapons.AIM_9M_Sidewinder_IR_AAM)
@@ -55,5 +73,11 @@ class F_23A(PlaneType):
 
     pylons: Set[int] = {1, 3, 4, 5, 6, 7, 8, 10}
 
-    tasks = [task.CAP, task.Escort, task.FighterSweep, task.Intercept, task.Reconnaissance]
+    tasks = [
+        task.CAP,
+        task.Escort,
+        task.FighterSweep,
+        task.Intercept,
+        task.Reconnaissance,
+    ]
     task_default = task.CAP
