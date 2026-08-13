c9281d93ddc8d04ea35a6883e905af04764a7bed
Drexyl <mcourt@konekt.com.au>
2026-08-13 20:00:07 +1000

    Update VSN Mirage III

--- resources/customized_payloads/VSN_MirageIII.lua
+++ resources/customized_payloads/VSN_MirageIII.lua
@@ -1,5 +1,5 @@
 local unitPayloads = {
-	["name"] = "VSN_MirageIIIC",
+	["name"] = "VSN_MirageIII",
 	["payloads"] = {
 		[1] = {
 			["name"] = "Retribution BARCAP",
@@ -9,34 +9,26 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{FD21B13E-57F3-4C2A-9F78-C522D0B5BCE1}",
+					["CLSID"] = "{VSN_M3C17_PTB}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "{FD21B13E-57F3-4C2A-9F78-C522D0B5BCE1}",
-					["num"] = 3,
-				},
-				[4] = {
 					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
 					["num"] = 5,
 				},
-				[5] = {
-					["CLSID"] = "{VSN_M3C3_PTB}",
+				[4] = {
+					["CLSID"] = "{FD21B13E-57F3-4C2A-9F78-C522D0B5BCE1}",
 					["num"] = 6,
 				},
-				[6] = {
+				[5] = {
 					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
 					["num"] = 7,
 				},
-				[7] = {
-					["CLSID"] = "{FD21B13E-57F3-4C2A-9F78-C522D0B5BCE1}",
-					["num"] = 9,
-				},
-				[8] = {
-					["CLSID"] = "{FD21B13E-57F3-4C2A-9F78-C522D0B5BCE1}",
+				[6] = {
+					["CLSID"] = "{VSN_M3C17_PTB}",
 					["num"] = 10,
 				},
-				[9] = {
+				[7] = {
 					["CLSID"] = "{FC23864E-3B80-48E3-9C03-4DA8B1D7497B}",
 					["num"] = 11,
 				},
@@ -48,6 +40,6 @@ local unitPayloads = {
 	},
 	["tasks"] = {
 	},
-	["unitType"] = "VSN_MirageIIIC",
+	["unitType"] = "VSN_MirageIII",
 }
 return unitPayloads
